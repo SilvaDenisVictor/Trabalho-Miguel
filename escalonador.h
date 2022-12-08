6aa3a1040b47fa4e037f4af692cb979e9c2f188d
josegerardoar <120033783+josegerardoar@users.noreply.github.com>
2022-12-08 20:20:32 -0300

    Update escalonador.h

--- escalonador.h
+++ escalonador.h
@@ -10,16 +10,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void e_inicializar (Escalonador *e, int caixas, int delta_t, int n_1, int n_2, int n_3, int n_4, int n_5);
-int e_inserir_por_fila (Escalonador *e, int classe, int num_conta, int qtde_operacoes);
-int e_obter_prox_num_conta(Escalonador *e);
-int e_consultar_prox_num_conta (Escalonador *e);
-int e_consultar_prox_qtde_oper (Escalonador *e);
-int e_consultar_prox_fila (Escalonador *e);
-int e_consultar_qtde_clientes (Escalonador *e);
-int e_consultar_tempo_prox_cliente (Escalonador *e);
-int e_conf_por_arquivo (Escalonador *e, char *nome_arq_conf);
-void e_rodar (Escalonador *e, char *nome_arq_in, char *nome_arq_out);
+void e_inicializar (Escalonador *, int, int, int , int , int , int, int);
+int e_inserir_por_fila (Escalonador *, int, int, int);
+int e_obter_prox_num_conta(Escalonador *);
+int e_consultar_prox_num_conta (Escalonador *);
+int e_consultar_prox_qtde_oper (Escalonador *);
+int e_consultar_prox_fila (Escalonador *);
+int e_consultar_qtde_clientes (Escalonador *);
+int e_consultar_tempo_prox_cliente (Escalonador *);
+int e_conf_por_arquivo (Escalonador *, char *);
+void e_rodar (Escalonador *, char *, char *);
 
 
 #endif /* escalonador_h */
