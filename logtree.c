f6d92aee9cf27ed7e2d0189ed6bf1f07f864bb34
josegerardoar <120033783+josegerardoar@users.noreply.github.com>
2022-12-08 10:26:11 -0300

    Update logtree.c

---
+++ logtree.c
@@ -0,0 +1,34 @@
+//Trabalho final
+//Grupo:
+//Jose Gerardo Araujo Rocha
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "logtree.h"
+
+void f_inicializar (Fila_FIFO **f);{
+
+}
+int f_inserir (Fila_FIFO **f, int chave, int valor);{
+
+}
+int f_obter_proxima_chave (Fila_FIFO **f);{
+
+}
+int f_consultar_proxima_chave (Fila_FIFO **f);{
+
+}
+int f_consultar_proximo_valor (Fila_FIFO **f);{
+
+}
+int f_num_elementos (Fila_FIFO **f);{
+
+}
+int f_consultar_chave_por_posicao (Fila_FIFO **f, int posicao);{
+
+}
+int f_consultar_valor_por_posicao (Fila_FIFO **f, int posicao);{
+
+}
