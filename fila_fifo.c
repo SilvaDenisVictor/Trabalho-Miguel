596b09c856992d612251dcae3d77a98ea5907211
joa1henrique <110608131+joa1henrique@users.noreply.github.com>
2022-12-08 18:17:16 -0300

    Update fila_fifo.c

--- fila_fifo.c
+++ fila_fifo.c
@@ -8,18 +8,50 @@
 #include <stdlib.h>
 #include "fila_fifo.h"
 
-void log_inicializar(Log **l);{
+typedef struct F
+{
+	int front;
+	int rear;
+	int counter;
+	Customer array[FSIZE];
+} F;
+
+void f_inicializar (Fila_FIFO **f);{
+  int i;
+	f->counter = 0;
+	f->front = 0;
+	f->rear = 0;
+	for (i = 0; i < FSIZE; i++)
+		CustInitialize( &(f->array[i]) );
+}
+int f_inserir (Fila_FIFO **f, int chave, int valor);{
+  if {
+    (f->counter == FSIZE)
+  }
+		return 0;
+	else
+	{
+		f->counter++;
+		CustAssign( &(f->array[f->rear]) ,valor);
+		f->rear = ( f->rear + 1 )% FSIZE;
+	}
+	return 1;
+}
+int f_obter_proxima_chave (Fila_FIFO **f);{
 
 }
-void log_registrar(Log **l, int conta, int classe, int timer, int caixa);{
+int f_consultar_proxima_chave (Fila_FIFO **f);{
+  return front *f;
+}
+int f_consultar_proximo_valor (Fila_FIFO **f);{
+  return front f;
+}
+int f_num_elementos (Fila_FIFO **f);{
+  return f->counter;
+}
+int f_consultar_chave_por_posicao (Fila_FIFO **f, int posicao);{
 
 }
-float log_media_por_classe(Log **l, int classe);{
-
-}
-int log_obter_soma_por_classe(Log **l, int classe);{
-
-}
-int log_obter_contagem_por_classe(Log **l, int classe);{
+int f_consultar_valor_por_posicao (Fila_FIFO **f, int posicao);{
 
 }
