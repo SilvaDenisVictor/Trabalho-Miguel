b2d04eae26c8fbcbfbcde8949305d8b565ae608b
josegerardoar <120033783+josegerardoar@users.noreply.github.com>
2022-12-08 10:23:12 -0300

    Update fila_fifo.c

---
+++ fila_fifo.c
@@ -0,0 +1,25 @@
+//Trabalho final
+//Grupo:
+//Jose Gerardo Araujo Rocha
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila_fifo.h"
+
+void log_inicializar(Log **l);{
+
+}
+void log_registrar(Log **l, int conta, int classe, int timer, int caixa);{
+
+}
+float log_media_por_classe(Log **l, int classe);{
+
+}
+int log_obter_soma_por_classe(Log **l, int classe);{
+
+}
+int log_obter_contagem_por_classe(Log **l, int classe);{
+
+}
