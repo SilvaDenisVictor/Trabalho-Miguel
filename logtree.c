29d9c781b5cba8027fb2a80804e48ae4f1e34d89
josegerardoar <120033783+josegerardoar@users.noreply.github.com>
2022-12-08 20:08:15 -0300

    Update logtree.c

--- logtree.c
+++ logtree.c
@@ -8,18 +8,18 @@
 #include <stdlib.h>
 #include "logtree.h"
 
-void log_inicializar(Log **l);{
+void log_inicializar(Log **l){
 
 }
-void log_registrar(Log **l, int conta, int classe, int timer, int caixa);{
+void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
 
 }
-float log_media_por_classe(Log **l, int classe);{
+float log_media_por_classe(Log **l, int classe){
 
 }
-int log_obter_soma_por_classe(Log **l, int classe);{
+int log_obter_soma_por_classe(Log **l, int classe){
 
 }
-int log_obter_contagem_por_classe(Log **l, int classe);{
+int log_obter_contagem_por_classe(Log **l, int classe){
 
 }
