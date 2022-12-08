dc07950c2e5310e99851e90a178afe8042fc40e5
josegerardoar <120033783+josegerardoar@users.noreply.github.com>
2022-12-08 09:53:10 -0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+//Trabalho final
+//Grupo:
+//Jose Gerardo Araujo Rocha
+//
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, const char * argv[]) {
+
+  
+  
+ return EXIT_SUCCESS;
+}
