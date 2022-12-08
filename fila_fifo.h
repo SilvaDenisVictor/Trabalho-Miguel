e3295b487a91449f3ce1a4619817e980fd281063
josegerardoar <120033783+josegerardoar@users.noreply.github.com>
2022-12-08 20:05:35 -0300

    Create fila_fifo.h

--- fila_fifo.h
+++ fila_fifo.h
@@ -2,7 +2,7 @@
 //Grupo:
 //Jose Gerardo Araujo Rocha
 //
-//
+
 
 #ifndef fila_fifo_h
 #define fila_fifo_h
@@ -10,10 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void log_inicializar(Log **l);
-void log_registrar(Log **l, int conta, int classe, int timer, int caixa);
-float log_media_por_classe(Log **l, int classe);
-int log_obter_soma_por_classe(Log **l, int classe);
-int log_obter_contagem_por_classe(Log **l, int classe);
+void log_inicializar(Log **);
+void log_registrar(Log **, int, int , int , int);
+float log_media_por_classe(Log **, int );
+int log_obter_soma_por_classe(Log **, int );
+int log_obter_contagem_por_classe(Log **, int );
 
 #endif /* fila_fifo_h */
